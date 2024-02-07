69a4fec28468a014d3d7772967161b97765c487b
Crazygamerjk <christopher.foerster@phorms.school>
2024-02-07 16:28:42 +0100

    First fix.

--- app/src/main/java/com/anon15683/haushaltsliste/fragments/SplashFragment.kt
+++ app/src/main/java/com/anon15683/haushaltsliste/fragments/SplashFragment.kt
@@ -3,33 +3,35 @@ package com.anon15683.haushaltsliste.fragments
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.anon15683.haushaltsliste.R
-import com.anon15683.haushaltsliste.databinding.FragmentPurchasesBinding
 
 class SplashFragment : Fragment() {
     private lateinit var navControl: NavController
-    private lateinit var binding: FragmentPurchasesBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         //inflate the layout for this fragment
-
-        binding = FragmentPurchasesBinding.inflate(inflater, container, false)
-        return binding.root
+        return inflater.inflate(R.layout.fragment_spash, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        init(view)
+
         Handler(Looper.myLooper()!!).postDelayed(kotlinx.coroutines.Runnable {
             navControl.navigate(R.id.action_splashFragment_to_homeFragment)
         }, 2000)
+    }
+    private fun init(view: View) {
+        navControl = Navigation.findNavController(view)
     }
 }
